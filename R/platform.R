ab54a4d6f5bad023de67d8548f04290e720acaf9
OganM <ogan.mancarcii@gmail.com>
2017-10-04 17:03:11 -0700

    some platform

--- R/platform.R
+++ R/platform.R
@@ -1,16 +1,31 @@
+#' All platforms
+#' 
+#' List all platforms
+#'
+#' @inheritParams filterArg
+#' @inheritParams queryLimit
+#' @inheritParams sortArg
+#'
+#' @return List of lists containing platform object.
 #' @export
-getPlatform = function(platform = NULL, offset = 0, sort = '+id'){
-    limit = 9999
-    if(!is.null(platform)){
-        platform = paste0(platform,'/')
-    } else{
-        platform = ''
-    }
-    url = glue::glue(gemmaBase(),'platforms/{platform}','?offset={offset}&limit={limit}&sort{URLencode(sort,reserved = TRUE)}')
+#'
+#' @examples
+allPlatforms = function(filter = NULL,
+                        offset = 0,
+                        limit = 20,
+                        sort = '+id'){
+    url = 
+        glue::glue(gemmaBase(),
+                   'platforms/?{queryLimit(offset,limit)}&{sortArg(sort)}&{filterArg(filter)}')
     content = getContent(url)
-    if(platform == ''){
-        names(content) = content %>% purrr::map_chr('shortName')
-    }
+    names(content) =  content %>% purrr::map_chr('shortName')
     return(content)
 }
 
+
+platformInfo = function(platform,
+                        request = NULL,
+                        ...,
+                        file = NULL){
+    
+}
