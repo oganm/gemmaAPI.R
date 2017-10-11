9293a0ac5e6f94dcd833fc8525019c5b939bdcc5
OganM <ogan.mancarcii@gmail.com>
2017-10-11 12:57:24 -0700

    documentation and export fix

---
+++ R/documentationDirectory.R
@@ -0,0 +1,35 @@
+#' Endpoint functions
+#'
+#' Basic API calls. These functions return mostly unaltered data from a given API endpoint.
+#' 
+#' \link{allDatasets}: List all datasets
+#' 
+#' \link{datasetInfo}: Get information about a specific dataset
+#' 
+#' \link{allPlatforms}: List all platforms
+#' 
+#' \link{platformInfo}: Get information about a specific platform
+#' 
+#' \link{geneInfo}: Get information about genes
+#' 
+#' \link{allTaxa}: List all taxa
+#' 
+#' \link{taxonInfo}: Get information about a specific taxon
+#' 
+#' \link{annotationInfo}: Query for annotations
+#' 
+#' @name endpointFunctions
+#' 
+NULL
+
+
+
+
+#' High Level Functions
+#'
+#' These functions return data compiled from multiple api calls 
+#' 
+#' 
+#' @name highLevelFunctions
+#' 
+NULL
---
+++ R/highLevelFunctions.R
@@ -0,0 +1,16 @@
+
+
+#' compileMetadata
+#' 
+#' Compiles metadata for a single dataset that includes sample and sample
+#' metadata.
+#'
+#' @param dataset 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+compileMetadata = function(dataset){
+    
+}
