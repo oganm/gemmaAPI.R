7dfbe8922e29a87721a3788e9eabb093cd0b871d
OganM <ogan.mancarcii@gmail.com>
2020-02-11 21:27:37 -0800

    add gemmaDebug option for returning API call paths to debug

--- R/misc.R
+++ R/misc.R
@@ -37,6 +37,9 @@ simplifyExpressionColnames = function(cn){
 # if content can't be converted into text, assumes it is a gzipped platform
 # annotation file
 getContent = function(url,file = NULL, return = TRUE,overwrite = FALSE){
+    if(!is.null(options('gemmaDebug'))$gemmaDebug && options('gemmaDebug')$gemmaDebug){
+        print(url)
+    }
     if(return == FALSE & overwrite == FALSE & !is.null(file) && file.exists(file)){
         warning(file,' exists. Skipping')
         return(NULL)
